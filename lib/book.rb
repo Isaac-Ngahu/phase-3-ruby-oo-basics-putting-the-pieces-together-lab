869fbc5e6c4e3cb1d9e0f354453686a3794fb828
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-06-28 22:23:30 +0300

    complete

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,27 @@
-
+class Book
+    attr_reader :title
+    attr_reader :author
+    attr_reader :page_count
+    attr_reader :genre
+    # attr_reader :page_count
+    # attr_reader :genre
+    def initialize(book_title)
+        @title = book_title
+        # @author_name = author_name
+        # @page_count = page_count
+        # @genre = book_genre
+    end
+    def author=(author_name)
+        @author = author_name
+    end
+    def page_count=(page_count)
+        @page_count = page_count
+    end
+    def genre=(book_genre)
+        @genre = book_genre
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,27 @@
 # Make your shoe class here!
+class Shoe
+    attr_reader :brand
+    attr_reader :color
+    attr_reader :size
+    attr_reader :material
+    attr_reader :condition
+    def initialize(brand)
+        @brand = brand
+    end
+    def color=(shoe_color)
+        @color = shoe_color
+    end
+    def size=(shoe_size)
+        @size = shoe_size
+    end
+    def material=(material)
+        @material = material
+    end
+    def condition=(condition)
+        @condition =condition
+    end
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
